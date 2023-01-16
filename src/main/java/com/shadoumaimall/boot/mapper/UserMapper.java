02caf454273741ddd8dcd8128964232fce3d45ed
fish <3303077472@qq.com>
2023-01-17 00:04:07 +0800

    mapper

---
+++ src/main/java/com/shadoumaimall/boot/mapper/UserMapper.java
@@ -0,0 +1,16 @@
+package com.shadoumaimall.boot.mapper;
+
+import com.shadoumaimall.boot.entity.User;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author 啊啊啊啊不吵吵
+ * @since 2023-01-16
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
